3a1e8fd8bfd204316f53d45ff288748ebd426e95
Mehtab66 <mehtabahmed7777777@gmail.com>
2025-12-13 17:50:02 +0500

    route backend added

---
+++ models/route.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const routeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    routeLink: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      publicId: String,
+      url: String,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Route", routeSchema);
