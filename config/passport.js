3a50aec4a2e19c1634d9d27e7e35311e39ed459e
Mehtab66 <mehtabahmed7777777@gmail.com>
2026-01-10 14:04:24 +0500

    pushed

--- config/passport.js
+++ config/passport.js
@@ -23,6 +23,7 @@ const initializeGoogleStrategy = () => {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: process.env.GOOGLE_CALLBACK_URL || "https://veloclique.com/auth/google/callback",
+            proxy: true,
           },
           async (accessToken, refreshToken, profile, done) => {
             try {
